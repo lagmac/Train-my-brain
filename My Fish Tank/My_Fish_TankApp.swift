4f0c8a042330328947b309bdeed755afd4e1b5bb
Gino <gino.preti@icloud.com>
2020-11-18 09:23:59 +0100

    Initial Commit

---
+++ My Fish Tank/My_Fish_TankApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Fish_TankApp.swift
+//  My Fish Tank
+//
+//  Created by Gino Preti on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct My_Fish_TankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
